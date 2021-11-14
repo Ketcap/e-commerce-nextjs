b3c3481bf92b2db8a71466407b28437b3b895816
Ömer Şahin <63051255+1sahinomer1@users.noreply.github.com>
2021-11-14 17:36:49 +0300

    add indexjs apolloprovider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ApolloProvider } from "@apollo/client";
+import client from "../utils/apollo-client";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
